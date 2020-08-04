e9ef198bdd15bc0185a58ec146e59436ef7ea357
M Ab <mab16992@gmail.com>
2020-08-04 14:49:18 +0500

    Passport facebook

--- authenticate.js
+++ authenticate.js
@@ -1,29 +1,11 @@
-// var passport = require('passport');
-// var LocalStrategy = require('passport-local').Strategy;
-// var User = require('./models/user');
-
-//passport.use(new LocalStrategy(User.authenticate()));
-
-// passport.use(new LocalStrategy(
-//     function(username, password, done) {
-//       User.findOne({ username: username }, function (err, user) {
-//         if (err) { return done(err); }
-//         if (!user) { return done(null, false); }
-//         if (!user.verifyPassword(password)) { return done(null, false); }
-//         return done(null, user);
-//       });
-//     }
-//   ));
-
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
+const FacebookTokenStrategy = require('passport-facebook-token');
+
 
 const config = require('./config');
 
@@ -35,30 +17,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-// exports.getToken = function(user){
-//     return jwt.sign(user, config.secretKey,
-//         {expiresIn: 3600});
-// }
 
-// let opts = {};
-// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// opts.secretOrKey = config.secretKey;
-
-// exports.jwtPassport = passport.use(new JwtStrategy(opts,
-//     (jwt_payload,done) => {
-//         console.log("JWT Payload",jwt_payload);
-//         User.findOne({_id: jwt_payload._id},(err,user)=>{
-//             if(err){
-//                 return done(err,false)   
-//             }else if(user){
-//                 return (null,user)
-//             }else{
-//                 return done(null,false);
-//             }
-//         });
-//     }));
-
-// exports.verifyUser = passport.authenticate('jwt',{session:false});
 
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey,
@@ -98,3 +57,28 @@ exports.verifyAdmin = (req,res,next) => {
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 
 
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+        clientID: config.facebook.clientId,
+        clientSecret:config.facebook.clientSecret},(accessToken,refreshToken,profile,done)=>{
+            User.findOne({facebookId:profile.id},(err, user)=>{
+                if(err){
+                    return done(err,false)
+                }
+                else if(!err && user != null){
+                    return done(null,user)
+                }else{
+                    user = new User({username:profile.displayName});
+                    user.facebookId = profile.id;
+                    user.firstName = profile.name.givenName;
+                    user.lastName = profile.name.familyName;
+                    user.save((err,user)=>{
+                        if(err){
+                            return done(err,false)
+                        }else{
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+        ));
--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 module.exports = {
 
     'secretKey' : '12345-87645-12345-09872',
-    'mongoUrl' : 'mongodb://localhost:27017/confusion'
-
+    'mongoUrl' : 'mongodb://localhost:27017/confusion',
+    'facebook':{
+    		clientId: '289964945663638',
+    		clientSecret:'e21305747ab6e043a7a59e26f0db7efa'
+    }
 
 }
